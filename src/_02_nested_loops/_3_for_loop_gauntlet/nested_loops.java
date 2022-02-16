6981bcdc2641d534e4e75ec1ccfbb4edffa3535a
student <student@ip-172-31-54-199.us-west-2.compute.internal>
2022-02-15 19:57:52 -0800

    Worked on nested for loops.

--- src/_02_nested_loops/_3_for_loop_gauntlet/nested_loops.java
+++ src/_02_nested_loops/_3_for_loop_gauntlet/nested_loops.java
@@ -4,15 +4,57 @@ public class nested_loops {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//0-100
 		 for(int q = 0; q < 101; q++) {
 			 System.out.println(q);
 		 }
-		 for(int w = 101; w > 0; w--) {
+		 //100-0
+		 for(int w = 101; w > -1; w--) {
 			 System.out.println(w);
 		 }
-		 for (int e = 0; e > 0; e++) {
-			 
+		 //0-100 even
+		 for (int e = 0; e < 101; e++) {
+			 if( e%2==0) {
+				 System.out.println(e);
+			 }
 		 }
+		 //1-99 odd
+		 for (int r = 1; r < 100; r++) {
+			 if( r%2!=0) {
+				 System.out.println(r);
+			 }
+		 }
+		 //0-500 label even/odd
+		 for (int t = 0; t < 501; t++) {
+			 if( t%2==0) {
+				 System.out.println(t + " is even");
+			 }
+			 else {
+				 System.out.println(t + " is odd");
+			 }
+		 }
+		 //0-777 all multiples of 7
+		 for (int y = 0; y < 778; y++) {
+			 if( y%7==0) {
+				 System.out.println(y);
+			 }
+		 }
+		 //year + age
+		 int age = 0;
+		 int year = 2022;
+		 for (int u = 2009; u < year+1; u++) {
+			 System.out.println(u + " " + age);
+			 age++;
+		 }
+		 //displaying 10,11,12,20,21,etc
+		 for(int i = 0; i < 3; i++) {
+			 System.out.println(i);
+			 for(int o = 0; o < 3; o++) {
+				 System.out.println(o);
+				 
+			 }
+		 }
+		 
 	}
 
 }
