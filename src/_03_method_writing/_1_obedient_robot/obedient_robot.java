4d40f3eed0b750d65b288df0c5bac7ef026a8ff3
student <student@ip-172-31-51-195.us-west-2.compute.internal>
2022-03-06 12:56:14 -0800

    Finished obedient robot and started Happy pet

--- src/_03_method_writing/_1_obedient_robot/obedient_robot.java
+++ src/_03_method_writing/_1_obedient_robot/obedient_robot.java
@@ -8,17 +8,32 @@ public class obedient_robot {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Robot rob = new Robot();
-		rob.setX(300);
-		rob.setY(300);
-		rob.penDown();
+		
 		String shape = JOptionPane.showInputDialog(null, "What shape should I draw square, circle, ot triangle?");
+		if (shape.equals("square")) {
+			square();
+		}
+		else if (shape.equals("triangle")) {
+			triangle();
+		}
+		else if (shape.equals("circle")) {
+			circle();
+		}
+}
+	public static void square() {
+		Robot rob = new Robot();
+		rob.setX(100);
+		rob.setY(500);
+		rob.penDown();
+		rob.setSpeed(500);
+		
 		String color = JOptionPane.showInputDialog(null, "What color should I use red, green, blue, or, black?");
+		
 		if (color.equals("red")) {
 			rob.setPenColor(255,0,0);
 		}
 		else if(color.equals("green")) {
-			rob.setPenColor(0,255,0);
+			rob.setPenColor(100,255,100);
 		}
 		else if(color.equals("blue")) {
 			rob.setPenColor(0,0,255);
@@ -26,10 +41,63 @@ public class obedient_robot {
 		else if(color.equals("black")) {
 			rob.setPenColor(0,0,0);
 		}
+		for (int q = 0; q < 4; q++) {
+		rob.move(400);
+		rob.turn(90);
+		}
+	}
+	
+	public static void triangle() {
+		Robot rob = new Robot();
+		rob.setX(100);
+		rob.setY(500);
+		rob.penDown();
+		rob.setSpeed(500);
 		
-}
-	public static void square() {
-
+		String color = JOptionPane.showInputDialog(null, "What color should I use red, green, blue, or, black?");
+		
+		if (color.equals("red")) {
+			rob.setPenColor(255,0,0);
+		}
+		else if(color.equals("green")) {
+			rob.setPenColor(100,255,100);
+		}
+		else if(color.equals("blue")) {
+			rob.setPenColor(0,0,255);
+		}
+		else if(color.equals("black")) {
+			rob.setPenColor(0,0,0);
+		}
+		for (int q = 0; q < 3; q++) {
+		rob.move(400);
+		rob.turn(120);
+		}
 	}
 
+	public static void circle() {
+		Robot rob = new Robot();
+		rob.setX(200);
+		rob.setY(300);
+		rob.penDown();
+		rob.setSpeed(1000);
+		
+		String color = JOptionPane.showInputDialog(null, "What color should I use red, green, blue, or, black?");
+		
+		if (color.equals("red")) {
+			rob.setPenColor(255,0,0);
+		}
+		else if(color.equals("green")) {
+			rob.setPenColor(100,255,100);
+		}
+		else if(color.equals("blue")) {
+			rob.setPenColor(0,0,255);
+		}
+		else if(color.equals("black")) {
+			rob.setPenColor(0,0,0);
+		}
+		for (int q = 0; q < 360; q++) {
+		rob.move(3);
+		rob.turn(1);
+		}
+	}
 }
--- src/_03_method_writing/_2_happy_pet/HappyPet.java
+++ src/_03_method_writing/_2_happy_pet/HappyPet.java
@@ -1,21 +1,24 @@
-package _03_method_writing._2_happy_pet;
+ package _03_method_writing._2_happy_pet;
+import java.util.Random;
+
 import javax.swing.JOptionPane;
 
 public class HappyPet {
 	
 	// 1. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	int hlv = 0;
+	Random rand = new Random();
 	public static void main(String[] args) {
 		// 2. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		String pet = JOptionPane.showInputDialog(null, "What pet would you like to buy?");
 		// 3. REPEAT steps 4 - 7 enough times to make your pet happy!
 		
 			// 4. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
 			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+					new String[] { "cuddle", "feed", "water","walk", "groom", "clean","punch" }, null);
 
 			// 6. Use user input to call the appropriate method created in step 5 below.
 
@@ -27,4 +30,8 @@ public class HappyPet {
 	// 5. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+
+public static void cuddle() {
+	hlv = hlv + rand;
+}
 }
